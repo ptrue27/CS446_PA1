0ee0f7666a24949eae758b5f3435cdaf0fbc9c54
Curtis <cburchfield@nevada.unr.edu>
2022-02-28 00:56:31 -0800

    Curtis-ls and clear commands working

--- ptruecburchfield_ShellScript.c
+++ ptruecburchfield_ShellScript.c
@@ -17,7 +17,7 @@ void promptUser(bool isBatch);
 void printError();
 int parseInput(char *input, char *splitWords[]);
 char *redirectCommand(char *special, char *line, bool *isRedirect, char *tokens[], char *outputTokens[]);
-char *executeCommand(char *cmd, bool *isRedirect, char* tokens[], char* outputTokens[],  bool *isExits);
+char *executeCommand(char *cmd, bool *isRedirect, char* tokens[], char* outputTokens[],  bool *isExits,int numTokens);
 char getLetter(char *str, int index);
 bool exitProgram(char *tokens[], int numTokens);
 void launchProcesses(char *tokens[], int numTokens, bool isRedirect);
@@ -45,6 +45,7 @@ int main(int argc, char *argv[])
     else if(argc == 1){
         batchmode = false;
         inputFile = stdin;
+        printf("THis is where input is");
     } 
     else{
         if(inputFile = fopen(argv[1],"r")){
@@ -64,6 +65,7 @@ int main(int argc, char *argv[])
         // Get cmd string from input, exit program if end of batchfile
         if(fgets(cmd, STR_MAX, inputFile) == NULL){
             // exitProgram();
+            printf("\nDOes htis execute\n");
         }
         if(batchmode){
             puts(cmd);
@@ -72,6 +74,10 @@ int main(int argc, char *argv[])
         // Parse cmd string into tokens
         numTokens = parseInput(cmd, cmdTokens);
         
+        printHelp(cmdTokens,numTokens);
+        isExit=exitProgram(cmdTokens,numTokens);
+        executeCommand(cmd,&isRedirect,cmdTokens,cmdTokens,&isExit,numTokens);
+
         // Execute command
         // char outFileName[STR_MAX] = executeCommand(cmd, &isRedirect, char* tokens[], char* outputTokens[],  &isExit); need to figure out tokens vs. outputTokens
             
@@ -80,13 +86,8 @@ int main(int argc, char *argv[])
 
     return 0;
 }
-
-/*   MOVED runShell() FUNCTIONALITY TO main() but kept these here if you were working on it
-//These two don'w work 100% right now, if you enter two arguements (help test)
-            //it will break them. I am gonna get it working, just had to take a break
-            exitShell = exitProgram(splitWords,splitWordsIndex);
-            printHelp(splitWords,splitWordsIndex);
-*/
+            // exitShell = exitProgram(splitWords,splitWordsIndex);
+            // printHelp(splitWords,splitWordsIndex);
 
 //-------------------------------------------------------------------------
 void promptUser(bool batchmode)
@@ -108,7 +109,7 @@ void promptUser(bool batchmode)
 //-------------------------------------------------------------------------
 void printError()
 {
-    printf("Shell Program Error Encountered");
+    printf("\nShell Program Error Encountered\n");
 }
 
 //-------------------------------------------------------------------------
@@ -121,46 +122,42 @@ int parseInput(char *input, char *splitWords[]){
         // printf(" %s\n",splitWords[wordInd]);//This is it displaying each word, kind of confusing
         splitWords[++wordInd] = strtok(NULL, " ");
     }
+    // for (int i=0;i<wordInd;i++)
+    // {
+    //     printf("%s",splitWords[i]);
+    // }
     return wordInd;
 }
 
 //-------------------------------------------------------------------------
 void printHelp(char *tokens[], int numTokens)
 {
-    int x=0;
-    while (x <numTokens)
+    if (strcmp(tokens[0],"help\n")==0 || strcmp(tokens[0],"Help\n")==0 || strcmp(tokens[0],"HELP\n")==0 || strcmp(tokens[0],"HELP")==0 || strcmp(tokens[0],"Help")==0 || strcmp(tokens[0],"help")==0)
     {
-        printf("%s",tokens[x]);
-        x++;
-    }   
-    if (strcmp(tokens[0],"help\n")==0 || strcmp(tokens[0],"Help\n")==0 || strcmp(tokens[0],"HELP\n")==0)
-    {
-        printf("\nhelp detected\n");
-        // if (numTokens > 1)
-        // {
-        //     printError();
-        //     printf("\nHelp command with other error\n");
-        // }
-        // else
-        // {
+        if (numTokens > 1)
+        {
+            printError();
+            printf("\nHelp command with other arguement\n\n");
+        }
+        else
+        {
             printf("Helpful info:\n");
             printf("***Type 'exit' to exit\n");
             printf("***Type 'ls' to see all available files and folders in current directory\n");
-            printf("Other\n");
-        // }
+            printf("***Other\n");
+        }
     }
 }
 
 //-------------------------------------------------------------------------
 bool exitProgram(char *tokens[], int numTokens)
 {
-    if (strcmp(tokens[0],"exit\n")==0 || strcmp(tokens[0],"Exit\n")==0 || strcmp(tokens[0],"EXIT\n")==0)
+    if (strcmp(tokens[0],"exit\n")==0 || strcmp(tokens[0],"Exit\n")==0 || strcmp(tokens[0],"EXIT\n")==0 || strcmp(tokens[0],"exit")==0 || strcmp(tokens[0],"Exit")==0 || strcmp(tokens[0],"EXIT")==0)
     {
-        printf("\nexit detected\n");
         if (numTokens > 1)
         {
             printError();
-            printf("\nExit command with other error\n");
+            printf("\nExit command with other arguement\n\n");
             return false;
         }
         else
@@ -188,9 +185,16 @@ void changeDirectories(char *tokens[], int numTokens){
 
 
 // not finished
-char *executeCommand(char *cmd, bool *isRedirect, char* tokens[], char* outputTokens[],  bool *isExits){
+char *executeCommand(char *cmd, bool *isRedirect, char* tokens[], char* outputTokens[],  bool *isExits,int numTokens){
     char *command = strdup(cmd);
-    strcat(command, "\n");
+    // strcat(command, "\n");
+    printf("%s",command);
+    // int x=0;
+    // while(command[x]!=NULL)
+    // {
+    //     x++;
+    // }
+    // command[x-1]=NULL;
     char outFileName[STR_MAX] = "";
 
     // Check if command is a redirect
@@ -198,6 +202,17 @@ char *executeCommand(char *cmd, bool *isRedirect, char* tokens[], char* outputTo
         *isRedirect = true;
         // return redirectCommand();
     }
-    *isRedirect = false;
+    else
+    {
+        *isRedirect = false;
+    }
+    launchProcesses(&command,numTokens,isRedirect);
+}
 
+
+void launchProcesses(char *tokens[], int numTokens, bool isRedirect)
+{
+    tokens[0][strlen(tokens[0])-1] = '\0';
+    // execvp("ls",argumentList);
+    execvp(tokens[0],tokens);
 }
